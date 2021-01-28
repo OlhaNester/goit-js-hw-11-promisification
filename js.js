3341d8b650e95bb1908f54c297a0158b353c841c
OlhaNester <olga_nv@ukr.net>
2021-01-28 17:39:33 +0200

    3-я задача

--- js.js
+++ js.js
@@ -1,52 +1,3 @@
-// Задание 1
-// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved"
-// через ms миллисекунд.Значением исполнившегося промиса должно быть то кол - во миллисекунд
-// которое передали во время вызова функции delay.
-
-const delay = (ms) => {
-  // Твой код
-};
-
-const logger = (time) => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
-
-// Задание 2
-// Перепиши функцию toggleUserState() так, чтобы она не использовала callback -
-// функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
-
-const users = [
-  { name: "Mango", active: true },
-  { name: "Poly", active: false },
-  { name: "Ajax", active: true },
-  { name: "Lux", active: false },
-];
-
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map((user) =>
-    user.name === userName ? { ...user, active: !user.active } : user
-  );
-
-  callback(updatedUsers);
-};
-
-const logger = (updatedUsers) => console.table(updatedUsers);
-
-/*
- * Сейчас работает так
- */
-toggleUserState(users, "Mango", logger);
-toggleUserState(users, "Lux", logger);
-
-/*
- * Должно работать так
- */
-toggleUserState(users, "Mango").then(logger);
-toggleUserState(users, "Lux").then(logger);
-
 // Задание 3
 // Перепиши функцию makeTransaction() так, чтобы она не использовала callback -
 //   функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
@@ -55,19 +6,21 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
 
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
-
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
+      if (canProcess) {
+        resolve(transaction.id, delay);
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
 };
+//makeTransaction({ transaction }).then(logSuccess).catch(logError);
 
 const logSuccess = (id, time) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
@@ -80,10 +33,10 @@ const logError = (id) => {
 /*
  * Работает так
  */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 /*
  * Должно работать так
  */
